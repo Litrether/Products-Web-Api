a2920ebf6af7ae13805b80d76a11d10d974da07a
HoneyBadger HoneyBadger <anakinsanakins@outlook.com>
2021-08-13 00:06:09 +0300

    off currency

--- CurrencyConverter/ExchangeRatesAbstractAPI/CurrencyApiConnectionERA.cs
+++ CurrencyConverter/ExchangeRatesAbstractAPI/CurrencyApiConnectionERA.cs
@@ -9,21 +9,19 @@ namespace CurrencyConverter.ExchangeRatesAbstractAPI
 
         public double GetExchangeRate(string currencyName)
         {
-            var url = @"https://exchange-rates.abstractapi.com/v1/live/?api_key=0996a12ad2d0439194a8d0780927ca10&base=USD";
-
-            string jsonResponse;
-
-            using(var webClient = new WebClient())
+            using (var webClient = new WebClient())
             {
-                jsonResponse = webClient.DownloadString(url);
+                //var url = @"https://exchange-rates.abstractapi.com/v1/live/?api_key=0996a12ad2d0439194a8d0780927ca10&base=USD";
+                
+                //var jsonResponse = webClient.DownloadString(url);
+
+                //var deserializedResponse = JsonSerializer.Deserialize<CurrencyERA>(jsonResponse);
+
+                //if (currencyName != null && deserializedResponse.ExchangeRates.ContainsKey(currencyName.ToUpper()))
+                //    return deserializedResponse.ExchangeRates[currencyName.ToUpper()];
+
+                return default(double);
             }
-
-            var deserializedResponse = JsonSerializer.Deserialize<CurrencyERA>(jsonResponse);
-
-            if (currencyName != null && deserializedResponse.ExchangeRates.ContainsKey(currencyName.ToUpper()))
-                return deserializedResponse.ExchangeRates[currencyName.ToUpper()];
-
-            return default(double);
         }
     }
 }
