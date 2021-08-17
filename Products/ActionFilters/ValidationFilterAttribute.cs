061bf6b01e1feb28896c2899cfa7712b3b28ba59
Litrether <anakinsanakins@gmail.com>
2021-08-17 10:49:28 +0300

    Revert "Revert "changes""
    This reverts commit f43c07ad37e4621f96dc5158ed7fe2338a18d251.

--- Products/ActionFilters/ValidationFilterAttribute.cs
+++ Products/ActionFilters/ValidationFilterAttribute.cs
@@ -18,13 +18,19 @@ namespace Products.ActionFilters
         {
             var action = context.RouteData.Values["action"];
             var controller = context.RouteData.Values["controller"];
+            var methodHasDtoParam = method == "PATCH" ||
+                                    method == "POST" ||
+                                    method == "PUT";
 
-            var param = context.ActionArguments.SingleOrDefault(x => x.Value.ToString().Contains("Dto")).Value;
-            if (param == null)
+            if (methodHasDtoParam)
             {
-                _logger.LogError($"Object sent from client is null. Controller: { controller }, action: { action }.");
-                context.Result = new BadRequestObjectResult($"Object is null. Controller: { controller }, action: { action }.");
-                return false;
+                var param = context.ActionArguments.SingleOrDefault(x => x.Value.ToString().Contains("Dto")).Value;
+                if (param == null)
+                {
+                    _logger.LogError($"Object sent from client is null. Controller: {controller}, action: { action}.");
+                    context.Result = new BadRequestObjectResult($"Object is null. Controller: { controller }, action: { action}.");
+                    return false;
+                }
             }
 
             if (context.ModelState.IsValid == false)
